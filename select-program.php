2587572d09cccc772ad656220ef8e97cef55c0cf
angelperezleon <25584631+angelperezleon@users.noreply.github.com>
2020-06-10 15:22:20 +0200

    Create select-program.php

---
+++ select-program.php
@@ -0,0 +1,79 @@
+<?php
+require_once 'dbconn.php';
+error_reporting(E_ALL & ~E_NOTICE);
+?>
+
+<!DOCTYPE html>
+<!--
+	Spooky2 Treatment Diary
+	https://www.angelperezleon.com/spooky2
+-->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Spooky2 Treatment Diary - Spooky2 Program lists</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="UTF-8">
+		<!-- -->
+	<link href="style3.css" rel="stylesheet">		
+	</head>
+	<body>
+	
+<!--  START - Display selected programlist BLOCK
+
+	<form method="post" accept-charset="utf-8" size="50">
+	<!-- Read  -->
+	<select name='programlist[]' size="1" id="formFoods" required>
+<?php
+	//header("Content-Type:text/html; charset=utf-8");
+	 //header("Content-Type: text/html;charset=UTF-8");
+	// Start - Make connection to DB server	
+	require_once 'dbconn.php';	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Fix by Marco Gasi special chars been displayed
+	// https://www.experts-exchange.com/questions/28967328/mysql-question-mysql-query-SET-NAMES-'utf8'.html
+	$conn->set_charset("utf8mb4");
+	
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	// End - Make connection to DB server
+
+	// Start - Display Spooky2 Program list from DB
+		$sqlrec = mysqli_query($conn, "SELECT programlist FROM programs");	
+			while ($row = $sqlrec->fetch_assoc()){
+		echo "<option value=\"$row[programlist]\">" . $row['programlist'] . "</option>";
+	}
+	// End - Display Spooky2 Program list from DB
+
+?>
+	</select>
+
+	<!-- Comment out submit button
+	<input type="submit" value="Submit">
+
+
+<?php
+//header("Content-Type:text/html; charset=utf-8");
+	
+	// Start - Display selected programlist
+	if (isset($_POST["programlist"]))
+	{
+		$programout = $_POST['programlist'];
+			echo "$programout";			
+			//header('Content-Type: text/html; charset=utf-8');
+					
+	// Change character set to utf8
+	//mysqli_set_charset($con,"utf8");
+	
+			//echo "$programout";
+			//echo mb_convert_encoding("$programout", 'UTF-8', 'UTF-16BE');
+	}
+	// Start - Display selected programlist
+?>
+END - Display selected programlist BLOCK -->
+
+</body>
+</html>
